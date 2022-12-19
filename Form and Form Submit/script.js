96c45b3410208d8f5732329204abd36e81a8bb0f
Zehra Şeren <94180168+zehraseren@users.noreply.github.com>
2022-12-19 14:14:06 +0300

    Delete Form and Form Submit directory

--- Form and Form Submit/script.js
+++
@@ -1,7 +0,0 @@
-let formDOM = document.querySelector("#userForm")
-formDOM.addEventListener('submit', formSubmit)
-
-function formSubmit (event) {
-    event.preventDefault()
-    console.log("Saved")
-}
