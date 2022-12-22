f701093286cebbe65990a6735cbd5b875ef49c56
Zehra Seren <fatmazehraseren@gmail.com>
2022-12-22 18:18:18 +0300

    js  was completed

---
+++ Login-Register/log-reg.js
@@ -0,0 +1,17 @@
+const x = document.getElementById("login");
+const y = document.getElementById("register");
+const z = document.getElementById("btn");
+const a = document.getElementById("title-register");
+
+function register () {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+    a.style.left = "-400px"
+}
+
+function login () {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0";
+}
