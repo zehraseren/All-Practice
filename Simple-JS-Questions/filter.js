7bde0c5bd32d670a752dc1ec6ed9cf0a79192c63
Zehra Seren <fatmazehraseren@gmail.com>
2023-01-12 00:11:31 +0300

    Question was added

--- Simple-JS-Questions/filter.js
+++ Simple-JS-Questions/filter.js
@@ -9,8 +9,17 @@ const person = [{
     languages: ["Java", "HTML"],
 }];
 
+/*
+
+Q1. Using the array above, use the filter() method to retrieve the person whose age is greater than 30.
+Q2. Fetch the person who knows JavaScript with the filter() method using the array above.
+
+*/
+
+/* A1. */
 const age30 = person.filter(person => person.age > 30);
 console.log(age30);
 
+/* A2. */
 const isKnownJS = person.filter(person => person.languages.includes("JavaScript"));
 console.log(isKnownJS);
---
+++ Simple-JS-Questions/for.js
@@ -0,0 +1,18 @@
+/* 
+
+Q1. Print all numbers between 20 and 40 to the console.
+Q2. Let's print the even numbers between 0 and 50 to the console.
+
+*/ 
+
+/* A1. */
+for (var i = 20; i < 40; i++) {
+    console.log(i);
+}
+
+/* A2. */
+for (var j = 0; j <50; j++) {
+    if (j % 2 == 0) {
+        console.log(j);
+    };
+};
